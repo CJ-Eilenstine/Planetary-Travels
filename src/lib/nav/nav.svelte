ac47b0895aa6d1e393c6dbd2153a6d2f97b98557
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-02-07 22:24:46 -0500

    Sign In

--- src/lib/nav/nav.svelte
+++ src/lib/nav/nav.svelte
@@ -4,11 +4,11 @@
 
 <nav>
 	<div class="mainNav">
-		<a href="/home">Home</a>
+		<a href="/">Home</a>
 		<a href="./browse">Browse</a>
 	</div>
 	<div class="logs">
-		<a href="./signIn">Sign In</a>
+		<a href="./signin">Sign In</a>
 		<p>||</p>
 		<a href="./signup">Sign Up</a>
 	</div>
---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import './signin.css';
+	import Nav from '$lib/nav/nav.svelte';
+</script>
+
+<Nav />
+
+<div class="signIn">
+	<form>
+		<h1>Sign In</h1>
+		<input type="Email" placeholder="Email" />
+		<input type="Password" placeholder="Password" />
+		<button>Sign In</button>
+	</form>
+</div>
