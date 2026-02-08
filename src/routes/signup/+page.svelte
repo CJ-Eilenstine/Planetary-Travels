a4196dc3ccab14ae9f954bffe0ef8ad9f0920f8f
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-02-07 23:35:55 -0500

    Signup small update

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -8,13 +8,13 @@
 <div class="signUp">
 	<form>
 		<h1>Sign Up</h1>
-		<input type="Email" placeholder="Email" />
-		<input type="Username" placeholder="Username" />
-		<input type="Password" placeholder="Password" />
-		<input type="Password" placeholder="Confirm Password" />
-		<input type="Date of Birth" placeholder="Date of Birth" />
-		<input type="Planet Origin" placeholder="Planet Origin" />
-		<input type="Species" placeholder="Species" />
+		<input type="Email" name="email" placeholder="Email" />
+		<input type="text" name="username" placeholder="Username" />
+		<input type="Password" name="password" placeholder="Password" />
+		<input type="Password" name="confirm_password" placeholder="Confirm Password" />
+		<input type="date" name="dob" placeholder="Date of Birth" />
+		<input type="text" name="planet_origin" placeholder="Planet Origin" />
+		<input type="text" name="species" placeholder="Species" />
 		<button>Sign Up</button>
 	</form>
 </div>
