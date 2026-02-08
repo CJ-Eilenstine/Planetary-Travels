fa18ebb04dc1127c1b31aa6af8f26e30ddc933ec
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-02-07 20:07:39 -0500

    Initial commit

---
+++ src/lib/nav/nav.svelte
@@ -0,0 +1,15 @@
+<script>
+	import './nav.css';
+</script>
+
+<nav>
+	<div class="mainNav">
+		<a href="/home">Home</a>
+		<a href="./browse">Browse</a>
+	</div>
+	<div class="logs">
+		<a href="./signIn">Sign In</a>
+		<p>||</p>
+		<a href="./signUp">Sign Up</a>
+	</div>
+</nav>
