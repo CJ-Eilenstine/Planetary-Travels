f961ac464fb309c29a0e9fc5aadd8a4a765c7290
fralcala <fralcala@iu.edu>
2026-02-19 22:35:41 -0500

    browseCard layout

---
+++ src/lib/browse cards/browseCard.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './browseCard.css';
+</script>
+
+<div class="browseCard">
+	<div class="image-holder"></div>
+	<div class="browseCardTextHolder">
+		<h2>Planet Name</h2>
+		<p>Rating</p>
+		<p>Price</p>
+	</div>
+</div>
