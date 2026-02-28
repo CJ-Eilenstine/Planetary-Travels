be404a52c210f80422f2e539fabf001dc87ebcff
fralcala <fralcala@iu.edu>
2026-02-28 12:07:02 -0500

    quick push for browse card testing

--- src/lib/browse cards/browseCard.svelte
+++ src/lib/browse cards/browseCard.svelte
@@ -1,12 +1,13 @@
 <script>
 	import './browseCard.css';
+	let { planetName, rating, price } = $props();
 </script>
 
 <div class="browseCard">
 	<div class="image-holder"></div>
 	<div class="browseCardTextHolder">
-		<h2>Planet Name</h2>
-		<p>Rating</p>
-		<p>Price</p>
+		<h2><code>{planetName}</code></h2>
+		<p><code>{rating}</code></p>
+		<p>{price}</p>
 	</div>
 </div>
--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -1,6 +1,11 @@
 <script>
+	import BrowseCard from '$lib/browse cards/browseCard.svelte';
 </script>
 
 <div class="browse">
 	<h1>browse</h1>
+
+	<BrowseCard planetName={'planet1'} rating={'3.5'} price={'price'} />
+
+	<BrowseCard planetName={'planet2'} rating={'4.5'} price={'price2'} />
 </div>
