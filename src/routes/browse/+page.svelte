03b42a8eea2b8d8789b0d7f5593b4fa8c378ee42
fralcala <fralcala@iu.edu>
2026-02-28 13:50:29 -0500

    Added 3rd browse card

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -8,4 +8,6 @@
 	<BrowseCard planetName={'planet1'} rating={'3.5'} price={'price'} />
 
 	<BrowseCard planetName={'planet2'} rating={'4.5'} price={'price2'} />
+
+	<BrowseCard planetName={'planet3'} rating={'4.7'} price={'price3'} />
 </div>
