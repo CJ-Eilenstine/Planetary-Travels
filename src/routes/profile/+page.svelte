a0eb71e1d9ae9e88d489ce35df8090061427edf4
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-02-28 23:03:25 -0500

    Profile Layout

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -25,10 +25,15 @@
 	<div class="profileCard">
 		{#if profileData}
 			<h1>Intergalactic Travel Administration</h1>
-			<h2>{profileData.username}</h2>
-			<h3>Planet: {profileData.planetOrigin}</h3>
-			<h3>Species: {profileData.species}</h3>
-			<h3>Date of Birth: {profileData.dateOfBirth}</h3>
+			<div class="userInfo">
+				<div class="char"></div>
+				<div class="info">
+					<h2>{profileData.username}</h2>
+					<h3>Planet: {profileData.planetOrigin}</h3>
+					<h3>Species: {profileData.species}</h3>
+					<h3>Date of Birth: {profileData.dateOfBirth}</h3>
+				</div>
+			</div>
 		{/if}
 	</div>
 </div>
