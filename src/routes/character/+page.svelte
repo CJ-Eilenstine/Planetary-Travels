7722eb1414df0a1f889b15899bd1170adee632e6
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-03-07 21:10:22 -0500

    Character layout

--- src/routes/character/+page.svelte
+++ src/routes/character/+page.svelte
@@ -3,7 +3,18 @@
 	import './character.css';
 </script>
 
-<div class="character-page">
-	<h1>Character Page</h1>
-	<p>This is the character page. Content will go here.</p>
+<div class="characterPage">
+	<h1>Your Appearance</h1>
+	<div class="characterEditor">
+		<div class="char"></div>
+		<h2>Click on the arrows to change your appearance.</h2>
+		<div class="charEditor">
+			<p>Hair</p>
+			<p>Head</p>
+			<p>Mouth</p>
+			<p>Eyes</p>
+			<p>Body</p>
+			<p>Nose</p>
+		</div>
+	</div>
 </div>
