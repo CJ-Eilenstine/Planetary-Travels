e498a90b3b17ced0f970499a31f5c4e2913ee88e
fralcala <fralcala@iu.edu>
2026-03-07 20:46:16 -0500

    Home page layout and basic css

---
+++ src/lib/home cards/homeCards.svelte
@@ -0,0 +1,13 @@
+<script>
+	import './homeCards.css';
+	let { planetName, rating, price } = $props();
+</script>
+
+<div class="homeCard">
+	<div class="image-holder"></div>
+	<div class="homeCardTextHolder">
+		<h2>{planetName}</h2>
+		<p>{rating}</p>
+		<p>{price}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
 	// You can add any necessary script logic here
 	import './home.css';
+	import HomeCard from '$lib/home cards/homeCards.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home">
+	<div class="banner">
+		<div class="bannerText"></div>
+	</div>
+
+	<div class="homeCardsSection">
+		<HomeCard planetName={'planet1'} rating={'3.5'} price={'price'} />
+
+		<HomeCard planetName={'planet2'} rating={'4.5'} price={'price2'} />
+
+		<HomeCard planetName={'planet3'} rating={'4.7'} price={'price3'} />
+	</div>
+</div>
