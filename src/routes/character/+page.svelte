5f3a00305ab6442c87b6acff06d00a4f9c44561e
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-03-14 18:53:31 -0400

    Arrows to change char stuff

--- src/routes/character/+page.svelte
+++ src/routes/character/+page.svelte
@@ -9,12 +9,36 @@
 		<div class="char"></div>
 		<h2>Click on the arrows to change your appearance.</h2>
 		<div class="charEditor">
-			<p>Hair</p>
-			<p>Head</p>
-			<p>Mouth</p>
-			<p>Eyes</p>
-			<p>Body</p>
-			<p>Nose</p>
+			<div class="charEditorHair">
+				<button>&lt;</button>
+				<p>Hair</p>
+				<button>&gt;</button>
+			</div>
+			<div class="charEditorHead">
+				<button>&lt;</button>
+				<p>Head</p>
+				<button>&gt;</button>
+			</div>
+			<div class="charEditorMouth">
+				<button>&lt;</button>
+				<p>Mouth</p>
+				<button>&gt;</button>
+			</div>
+			<div class="charEditorEyes">
+				<button>&lt;</button>
+				<p>Eyes</p>
+				<button>&gt;</button>
+			</div>
+			<div class="charEditorBody">
+				<button>&lt;</button>
+				<p>Body</p>
+				<button>&gt;</button>
+			</div>
+			<div class="charEditorNose">
+				<button>&lt;</button>
+				<p>Nose</p>
+				<button>&gt;</button>
+			</div>
 		</div>
 	</div>
 </div>
