b9dbbb11024e1dc63474ca82da28709d25837c9a
fralcala <fralcala@iu.edu>
2026-04-13 20:11:48 -0400

    Revert "Merge branch 'main' of https://github.com/CJ-Eilenstine/Planetary-Travels"
    This reverts commit f718cca3328cecd5ce3afaec892bb6dd4dec5979, reversing
    changes made to b9706293663a88daf95b1a8e46dd555e2181d08e.

--- src/routes/browse/[slug]/+page.svelte
+++ src/routes/browse/[slug]/+page.svelte
@@ -6,6 +6,25 @@
 		reviewText = '';
 
 	let { data } = $props();
+
+	function createReview() {
+		document.getElementById('userInput').innerHTML =
+			'<div class=userInputForm><p>Write your review. How was your trip?</p><input bind:value={reviewTitle} placeholder="Review Title" type="text" class="reviewTitle"/><textarea bind:value={reviewText} placeholder="Your review :)" type="text" class="reviewText"></textarea><button onclick={saveReview}>Save</button></div>';
+	}
+
+	function saveReview() {
+		console.log('save');
+		// if (!reviewTitle || !reviewText) {
+		// 	alert('Please fill in all fields');
+		// 	return;
+		// }
+
+		// setDoc(doc(db, 'reviews', review.uid), {
+		// 	reviewTitle,
+		// 	reviewText,
+		// 	createdAt: new Date()
+		// });
+	}
 </script>
 
 <main class="detailsPage">
@@ -17,9 +36,9 @@
 		<h2>{data.detail.rating}</h2>
 		<h3>{data.detail.price}</h3>
 		<p>{data.detail.desc}</p>
-		<button onclick={() => (showModal = true)}>Travel Now!!!</button>
+		<button>Travel Now!!!</button>
 	</div>
-	<div class="reviews">
+	<div class="reviews" id="reviews">
 		<h4>Reviews</h4>
 		<button onclick={createReview}>Leave a review</button>
 		<div id="userInput"></div>
