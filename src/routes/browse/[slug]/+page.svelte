033ad737598abf5210860d70f4c903865637cafc
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-04-13 20:59:12 -0400

    Modal

--- src/routes/browse/[slug]/+page.svelte
+++ src/routes/browse/[slug]/+page.svelte
@@ -1,6 +1,9 @@
 <script>
 	import './details.css';
 	let { data } = $props();
+	import { Modal } from '$lib';
+
+	let showModal = $state(false);
 </script>
 
 <main class="detailsPage">
@@ -12,8 +15,33 @@
 		<h2>{data.detail.rating}</h2>
 		<h3>{data.detail.price}</h3>
 		<p>{data.detail.desc}</p>
-		<button>Travel Now!!!</button>
+		<button onclick={() => (showModal = true)}>Travel Now!!!</button>
 	</div>
+	<Modal bind:showModal>
+		{#snippet header()}
+			<h2>
+				modal
+				<small><em>adjective</em> mod·al \ˈmō-dəl\</small>
+			</h2>
+		{/snippet}
+
+		<ol class="definition-list">
+			<li>of or relating to modality in logic</li>
+			<li>
+				containing provisions as to the mode of procedure or the manner of taking effect —used of a
+				contract or legacy
+			</li>
+			<li>of or relating to a musical mode</li>
+			<li>of or relating to structure as opposed to substance</li>
+			<li>
+				of, relating to, or constituting a grammatical form or category characteristically
+				indicating predication
+			</li>
+			<li>of or relating to a statistical mode</li>
+		</ol>
+
+		<a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+	</Modal>
 	<div class="reviews">
 		<h4>Reviews</h4>
 		<button>Leave a review</button>
