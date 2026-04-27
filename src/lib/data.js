d896ae2f22cc30f8c459179df5c1af1c7ede6687
CJ-Eilenstine <cj.eilenstine@gmail.com>
2026-04-27 16:55:29 -0400

    update css

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,7 +4,7 @@ export const details = [
 		planetName: 'Solaris',
 		rating: '1.2',
 		price: '𝄽1,253',
-		image: './src/lib/assets/planets/solaris.gif',
+		image: './assets/planets/solaris.gif',
 		desc: "Solaris, a planet mostly covered by a 700 billion tons sentient gelatinous ocean. Come see tall extensors, complex shaped symmetriads, and mimoids that extrude masses of plasma that mimic the shapes of other things from clouds to human made object. Catch a peek of these temporary structures before they dissolve back into the ocean. Maybe you'll even see some rare free floating independents that detach themselves from the surface of Solaris. Be aware that there is no oxygen and Solaris' atmosphere is considered very toxic."
 	},
 
